806dcc7a17fc6712328e681f77374b2698400aa0
zhanggeng <m13716920635@163.com>
2017-11-23 15:58:07 +0800

    RunLoopHTTPRequest

--- AuidoDemo/ViewController.m
+++ AuidoDemo/ViewController.m
@@ -7,15 +7,36 @@
 //
 
 #import "ViewController.h"
+#import "WDSimpleHTTPRequest.h"
 
 @interface ViewController ()
-
+@property (nonatomic,strong) WDSimpleHTTPRequest * manager;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.manager = [WDSimpleHTTPRequest requestWithURL:[NSURL URLWithString:@"http://mr1.doubanio.com/27fb317e112de158a9d52293d18e7b14/0/fm/song/p372_128k.mp4"]];
+ 
+    [_manager setCompletedBlock:^{
+        NSLog(@"complete");
+       // [_self _requestDidComplete];
+    }];
+    
+    [_manager setProgressBlock:^(double downloadProgress) {
+         NSLog(@"progress %f",downloadProgress);
+    }];
+    
+    [_manager setDidReceiveResponseBlock:^{
+      
+    }];
+    
+    [_manager setDidReceiveDataBlock:^(NSData *data) {
+         NSLog(@"data==%@",data);
+    }];
+    [self.manager start];
+    
     // Do any additional setup after loading the view, typically from a nib.
 }
 
