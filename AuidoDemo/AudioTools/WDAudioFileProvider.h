083f2aca66f12789893fbd6f1b583415b5467e63
zhanggeng <m13716920635@163.com>
2017-11-23 16:47:07 +0800

    音频文件

---
+++ AuidoDemo/AudioTools/WDAudioFileProvider.h
@@ -0,0 +1,13 @@
+//
+//  WDAudioFileProvider.h
+//  AuidoDemo
+//
+//  Created by VD on 2017/11/23.
+//  Copyright © 2017年 VD. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WDAudioFileProvider : NSObject
+
+@end
---
+++ AuidoDemo/AudioTools/WDAudioFileProvider.m
@@ -0,0 +1,15 @@
+
+
+//
+//  WDAudioFileProvider.m
+//  AuidoDemo
+//
+//  Created by VD on 2017/11/23.
+//  Copyright © 2017年 VD. All rights reserved.
+//
+
+#import "WDAudioFileProvider.h"
+
+@implementation WDAudioFileProvider
+
+@end
